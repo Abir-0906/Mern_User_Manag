94c4bcdec41c523cc20399ba873ecbe1dff61b13
Abir Banik <abirbanik65@gmail.com>
2025-07-31 22:07:37 +0530

    udated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://mern-user-manag-2.onrender.com/api', // ✅ Use your Render backend URL
 });
 
 export default API;
